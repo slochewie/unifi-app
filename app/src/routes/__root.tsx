348764560e15690cdf01c314f23f65fbf2077008
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-08-30 18:25:40 -0700

    Wrap app in NiteOwl chrome

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -2,15 +2,16 @@ import {
   HeadContent,
   Scripts,
   createRootRouteWithContext,
-} from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+} from "@tanstack/react-router"
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
+import { TanStackDevtools } from "@tanstack/react-devtools"
 
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+import { AppChrome } from "#/components/app-chrome.tsx"
+import TanStackQueryDevtools from "../integrations/tanstack-query/devtools"
 
-import appCss from '../styles.css?url'
+import appCss from "../styles.css?url"
 
-import type { QueryClient } from '@tanstack/react-query'
+import type { QueryClient } from "@tanstack/react-query"
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -20,19 +21,19 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   head: () => ({
     meta: [
       {
-        charSet: 'utf-8',
+        charSet: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       {
-        title: 'TanStack Start Starter',
+        title: "NiteOwl.dev UniFi Network Status",
       },
     ],
     links: [
       {
-        rel: 'stylesheet',
+        rel: "stylesheet",
         href: appCss,
       },
     ],
@@ -47,14 +48,14 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        {children}
+        <AppChrome>{children}</AppChrome>
         <TanStackDevtools
           config={{
-            position: 'bottom-right',
+            position: "bottom-right",
           }}
           plugins={[
             {
-              name: 'Tanstack Router',
+              name: "Tanstack Router",
               render: <TanStackRouterDevtoolsPanel />,
             },
             TanStackQueryDevtools,
